34cc95f00c8dfea500d386dc514aa5036021d96d
dranchukroma <roman2003dranchuk@gmail.com>
2026-05-22 12:44:46 +0200

    Updated README

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.tt1cs365u6o"
+    "revision": "0.69nbhtbap4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
